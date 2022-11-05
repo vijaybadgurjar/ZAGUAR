d308e5d7ef0a7cd73a88546d37bf788375267342
vijaybadgurjar <92264840+vijaybadgurjar@users.noreply.github.com>
2022-11-05 11:08:16 +0530

    keys added

---
+++ keys.js
@@ -0,0 +1,4 @@
+module.exports={
+   MONGOURI:"mongodb+srv://ZAGUAR:TEST1234@cluster0.7rrlujm.mongodb.net/?retryWrites=true&w=majority",
+   JWT_SECRET:"fslkdjfsldfvijay" // when user will signed we give to user a token 
+}
